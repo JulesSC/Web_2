0e1795b2ef26f362441f00f504ad59666d92b770
JulesSC <81724365+JulesSC@users.noreply.github.com>
2021-05-18 15:28:59 -0400

    okay we're doing better now

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,16 +1,16 @@
-// import $ from "jquery";
+import $ from "jquery";
 
-// let $nav = $('nav[data-nav]');
-// let isVisible = false;
+let $nav = $('nav[data-nav]');
+let isVisible = false;
 
-// $(".nav-btns").on("click", function(){
-//     // console.log("click");
-//     if(isVisible === false){
-//         $nav.show();
-//         isVisible = true;
-//     }
-//     else{
-//         $nav.hide();
-//         isVisible = false;
-//     }
-// })
+$(".nav-btns").on("click", function(){
+    // console.log("click");
+    if(isVisible === false){
+        $nav.show();
+        isVisible = true;
+    }
+    else{
+        $nav.hide();
+        isVisible = false;
+    }
+})
